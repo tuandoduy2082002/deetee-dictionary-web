545c90529acd475eb02fcec46eb95e19969e9320
DeeTee <tuandoduy2082002@gmail.com>
2021-11-09 14:35:36 +0700

    update

--- routes/dictionary.js
+++ routes/dictionary.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    res.render('home')
+router.get('/',(req,res) => {
+    res.send('Hello')
 })
 
 module.exports = router
--- sever.js
+++ sever.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
-const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+
+const homeRouter = require('./routes/home')
+const dictionaryRouter = require('./routes/dictionary')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+app.use('/', homeRouter)
+app.use('/dictionary', dictionaryRouter)
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Sever is listen on port', process.env.PORT || 3000)
+})
